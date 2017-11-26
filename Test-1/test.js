d4dfa7ab3d442516c5664d258866877c911d2adf
Anthony <34007392+agh2623@users.noreply.github.com>
2017-11-26 17:52:07 +0000

    Initial Node-JS stuff
    Added Node-JS(Read/write from file)

--- Test-1/test.js
+++ Test-1/test.js
@@ -1,2 +1,9 @@
-g
-rrr
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(80);
